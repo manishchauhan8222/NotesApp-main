9b3e898f4035d7f7c761206584e2422e5c09c61a
Manish Chauhan <manishchauhan8222@gmail.com>
2025-05-28 03:32:58 +0530

    again

--- Frontend/src/services/ApiEndPoint.js
+++ Frontend/src/services/ApiEndPoint.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
-    headers: {
-        'Content-Type': 'application/json' // Removed extra space
-    },
-    withCredentials: true
+  baseURL: "https://notesapp-main-3.onrender.com/",
+  headers: {
+    "Content-Type": "application/json", // Removed extra space
+  },
+  withCredentials: true,
 });
 
 export const get = (url, params) => instance.get(url, { params });
@@ -14,23 +14,29 @@ export const put = (url, data) => instance.put(url, data);
 export const delet = (url) => instance.delete(url);
 
 // Add a request interceptor
-instance.interceptors.request.use(function (config) {
+instance.interceptors.request.use(
+  function (config) {
     // Log the request config for debugging
-    console.log('Request Config:', config);
+    console.log("Request Config:", config);
     return config;
-}, function (error) {
+  },
+  function (error) {
     // Do something with request error
-    console.error('Request Error:', error);
+    console.error("Request Error:", error);
     return Promise.reject(error);
-});
+  }
+);
 
 // Add a response interceptor
-instance.interceptors.response.use(function (response) {
+instance.interceptors.response.use(
+  function (response) {
     // Log the response data for debugging
-    console.log('Apis Response', response);
+    console.log("Apis Response", response);
     return response;
-}, function (error) {
+  },
+  function (error) {
     // Log the error message for debugging
-    console.log('Api Error', error.message);
+    console.log("Api Error", error.message);
     return Promise.reject(error);
-});
+  }
+);
