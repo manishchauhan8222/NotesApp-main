b214faca7801dc6479a5437ff276d908d31ee487
Manish Chauhan <manishchauhan8222@gmail.com>
2025-05-28 03:21:27 +0530

    third

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-const DbCon=async()=>{
-    try {
-        mongoose.connect("mongodb://localhost:27017/Notes_App")
-        console.log('Mongodb is connected')
-        
-    } catch (error) {
-        console.log("Error in mongodb connection",error)
-    }
-}
-export default DbCon
+const DbCon = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://manishchauhan822282:ManishDatabase@cluster0.5sfp22x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log("MongoDB is connected");
+  } catch (error) {
+    console.log("Error in MongoDB connection", error);
+  }
+};
+
+export default DbCon;
